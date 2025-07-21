78afaeb82eebcc8158cfa5ed13cf2fe4a2bdcc0f
mkaasvh <masha.kaverina.72@mail.ru>
2025-07-21 12:04:01 +0800

    added my changes

--- MathTrainer/ScoreManager.swift
+++ MathTrainer/ScoreManager.swift
@@ -5,25 +5,25 @@
 //  Created by Mariia on 20.07.2025.
 //
 
-//import Foundation
-//
-//enum MathTypes: Int {
-//    case add, subtract, multiply, divide
-//}
-//
-//final class ScoreManager {
-//    static let shared = ScoreManager()
-//    private init() {}
-//    
-//    var scoreDictionary: [MathTypes : Int] = [:]
-//    
-//    func addPoint(_ type: MathTypes) {
-//        scoreDictionary[type, default: 0] += 1
-//    }
-//    
-//    func getScore(_ type: MathTypes) -> Int {
-//        return scoreDictionary[type, default: 0]
-//    }
-//    
-//    
-//}
+import Foundation
+
+enum MathTypes: Int {
+    case add, subtract, multiply, divide
+}
+
+final class ScoreManager {
+    static let shared = ScoreManager()
+    private init() {}
+    
+    var scoreDictionary: [MathTypes : Int] = [:]
+    
+    func addPoint(_ type: MathTypes) {
+        scoreDictionary[type, default: 0] += 1
+    }
+    
+    func getScore(_ type: MathTypes) -> Int {
+        return scoreDictionary[type, default: 0]
+    }
+    
+    
+}
